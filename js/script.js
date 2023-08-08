a2611d7047f12e3314e6f030474d7b51e648a3a7
sujie-lee <21800528@handong.edu>
2023-08-09 00:01:30 +0900

    before review

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-/* 59-Fixing mobile nav menu atomatically collapsed */
+
 $(function () {
     $("#navbarToggle").blur(function (event) {
         var screenWidth = window.innerWidth;
@@ -11,9 +11,6 @@ $(function () {
 (function (global) { /* IIFE : immediately invoked function exp */
 
     var bdal = {};
-    // var homeHtml = "snippet/home-snippet.html"
-    var publicationUrl = "BDAlab_publications.json";
-    var publicationHtml = "publication.html";
 
     var insertHtml = function (selector, html) {
         var targetElem = document.querySelector(selector);
@@ -25,63 +22,14 @@ $(function () {
         insertHtml(selector, html);
     };
 
-    /*
-    // Return substitute of "{{propName}}" with propValue in given "string"
-    var insertProperty = function (string, propName, propValue) {
-        var propToReplace = "{{" + propName + "}}";
-        string = string.replace(new RegExp(propToReplace, "g"), propValue);
-        return string;
-    }
-    */
     // On page load (before images or CSS)
     document.addEventListener("DOMContentLoaded", 
         function (event) {
             
             showLoading("#main-content");
-
-            /*
-            $ajaxUtils.sendGetRequest(
-                homeHtml,
-                function (responseText) {
-                    document.querySelector("#main-content").innerHTML = responseText;
-                },
-                false);
-            */
         }
     );
-    /*
-    bdal.loadPublication = function () {
-        $ajaxUtils.sendGetRequest(publicationUrl, buildAndShow);
-    };
-    function buildAndShow (categories) {
-        $ajaxUtils.sendGetRequest(
-            publicationHtml,
-            function (publicationHtml) {
-                var publicationViewHtml = buildPublicationViewHtml (
-                    categories, publicationHtml);
-                    insertHtml("table-group-divider", publicationViewHtml);
-            },
-            false
-        );
-    }
-    function buildPublicationViewHtml (categories, publicationHtml) {
-        
-        for (var i=0; i<categories.length; i++) {
-            var html = publicationHtml;
-
-            var num = categories[i].Number;
-            var year = categories[i].Year;
-            var journal = categories[i].Journal;
-            var title = categories[i].Title;
-
-            html = insertProperty(html, "Number", num);
-            html = insertProperty(html, "Year", year);
-            html = insertProperty(html, "Journal", journal);
-            html = insertProperty(html, "Title", title);
-
-        }
-    }
-    */
+    
     global.$bdal = bdal;
 
 }) (window);
